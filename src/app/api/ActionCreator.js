9b027a4599ca15bf7316a6886097ec0ea9b20557
Guru107 <guruak107@gmail.com>
2017-09-02 02:59:37 +0530

    lint fixed

--- src/app/api/ActionCreator.js
+++ src/app/api/ActionCreator.js
@@ -12,28 +12,27 @@ const apiMap = {
 	NOW_SHOWING: {
 		url: `https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}&language=en-IN&page=1`,
 		successAction: actions.receivedNowShowing,
-		key : 'nowShowing'
+		key: "nowShowing"
 	},
 
 	TOP_RATED: {
 		url: `https://api.themoviedb.org/3/movie/top_rated?api_key=${TMDB_API_KEY}&language=en-IN&page=1`,
 		successAction: actions.receivedTopRated,
-		key : 'topRated'
+		key: "topRated"
 	},
 
 	POPULAR: {
 		url: `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-IN&page=1`,
 		successAction: actions.receivedPopular,
-		key : 'popular'
+		key: "popular"
 	}
 }
 
 export function getMovies(type) {
 	return (dispatch, getState) => {
-		
 		const actionMap = apiMap[type]
 
-		if( getState().moviesReducer[actionMap.key].length > 0 ) {
+		if (getState().moviesReducer[actionMap.key].length > 0) {
 			return
 		}
 
