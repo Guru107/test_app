9692ef666196c17defffe392203461883a56c9cc
Guru107 <guruak107@gmail.com>
2017-09-02 01:20:41 +0530

    redux thunk added

--- src/app/store.js
+++ src/app/store.js
@@ -1,19 +1,22 @@
 import { createStore, applyMiddleware, compose } from "redux"
 import { routerMiddleware } from "preact-router-redux"
 import rootReducer from "./reducer"
+import thunkMiddleware from "redux-thunk"
 export default function(history, state) {
 	let composeStore
 
 	if (__DEV__) {
 		composeStore = compose(
-			applyMiddleware(routerMiddleware(history)),
+			applyMiddleware(thunkMiddleware, routerMiddleware(history)),
 			typeof window === "object" &&
 			typeof window.devToolsExtension !== "undefined"
 				? window.devToolsExtension()
 				: f => f
 		)
 	} else {
-		composeStore = compose(applyMiddleware(routerMiddleware(history)))
+		composeStore = compose(
+			applyMiddleware(thunkMiddleware, routerMiddleware(history))
+		)
 	}
 	const store = createStore(rootReducer, state, composeStore)
 	return store
